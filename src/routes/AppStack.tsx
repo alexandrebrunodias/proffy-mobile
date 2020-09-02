e0e4542ac060667ff21f4dfef55b45e2518717b6
Alexandre Dias <alexbpdias@gmail.com>
2020-09-01 22:53:14 -0300

    Criada base do projeto

---
+++ src/routes/AppStack.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack'; 
+
+import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function AppStack() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={ { headerShown: false } }>
+        <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppStack;
